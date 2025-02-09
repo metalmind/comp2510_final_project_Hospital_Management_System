463bf322e3ce9d2b2daf6d2bb1c949aacc329d81
Alison Kim <kimalison99@gmail.com>
2025-02-09 02:23:04 -0800

    fix: implement input validation checks & modularize add and search patient functions
    Accounted for cases where user enters a String when scanf is expecting an integer - one call of getChar() is not sufficient to clear the input buffer in this case, so I added clearInputBuffer(). Currently searchForPatientRecord() does not have proper validation checks for the menu as I'm calling getSelection() from menu.c, which does not have access to clearInputBuffer(). I'm too scared to mess with the files at this point in time so this will be addressed in a future version.

--- src/main.c
+++ src/main.c
@@ -11,12 +11,18 @@
 #define MIN_AGE_YEARS 0
 
 #define TERMINAL_CHAR 0
-#define ID_NOT_FOUND -1
 #define ID_MIN_VALUE 0
+#define ID_NOT_FOUND (-1)
+
+#define INVALID_INPUT (-1)
+#define READ_SUCCESS 1
+
 #define ROOM_NUMBER_MIN 1
 
 #define SEARCH_BY_ID 1
 #define SEARCH_BY_NAME 2
+#define RETURN_TO_MENU 3
+
 #define RECORD_FOUND 0
 
 struct patient
@@ -30,9 +36,15 @@ struct patient
 
 void addNewPatientRecord(void);
 int idExists(struct patient arr[], int size, int id);
+int getPatientID(void);
+int getPatientAge(void);
+int getPatientRoomNumber(void);
 void searchForPatientRecord(void);
+int searchPatientByID(void);
+int searchPatientByName(void);
 void printPatientRecord(int id);
 void viewAllPatientRecords(void);
+void clearInputBuffer(void);
 
 struct patient patientRecords[MAX_PATIENTS];
 int totalPatients = 0;
@@ -97,40 +109,19 @@ void addNewPatientRecord(void)
     char diagnosis[DIAGNOSIS_MAX_CHAR];
     int roomNumber;
 
-    printf("Enter the ID of the patient (positive integer): ");
-    scanf("%d", &id);
-    getchar();
-
-    if(id < ID_MIN_VALUE ||
-        idExists(patientRecords, totalPatients, id) != ID_NOT_FOUND)
-    {
-        printf("Invalid or duplicate patient ID!\n");
-        return;
-    }
+    id = getPatientID();
 
     printf("Enter patient name: ");
     fgets(name, NAME_MAX_CHAR, stdin);
     name[strcspn(name, "\n")] = TERMINAL_CHAR;
 
-    do
-    {
-        printf("Enter patient age: ");
-        scanf("%d", &age);
-        getchar();
-    }
-    while(age < MIN_AGE_YEARS);
+    age = getPatientAge();
 
     printf("Enter patient diagnosis: ");
     fgets(diagnosis, DIAGNOSIS_MAX_CHAR, stdin);
     diagnosis[strcspn(diagnosis, "\n")] = TERMINAL_CHAR;
 
-    do
-    {
-        printf("Enter patient room number: ");
-        scanf("%d", &roomNumber);
-        getchar();
-    }
-    while(roomNumber < ROOM_NUMBER_MIN);
+    roomNumber = getPatientRoomNumber();
 
     patientRecords[totalPatients].patientID = id;
     strcpy(patientRecords[totalPatients].name, name);
@@ -143,62 +134,203 @@ void addNewPatientRecord(void)
 }
 
 /**
+ * Prompts user for patient ID, with checks to ensure patient ID is valid
+ * according to the following criteria:
+ * 1) Patient ID is a positive integer (greater than or equal to ID_MIN_VALUE)
+ * 2) Patient with the specified ID does not already exist
+ *
+ * @return patient ID
+ */
+int getPatientID()
+{
+    int id;
+
+    id = ID_NOT_FOUND;
+
+    do
+    {
+        printf("Enter patient ID: ");
+
+        // scanf returns number of items successfully assigned
+        int input = scanf("%d", &id);
+
+        if(input != READ_SUCCESS ||
+            id < ID_MIN_VALUE ||
+            (idExists(patientRecords, totalPatients, id) != ID_NOT_FOUND))
+        {
+            printf("Invalid or duplicate patient ID! Please enter a positive integer.\n");
+        }
+
+        clearInputBuffer();
+    }
+    while(id < ID_MIN_VALUE);
+
+    return id;
+}
+
+/**
+ * Prompts user for patient age, with checks to ensure patient age is valid
+ * according to the following criteria:
+ * 1) Patient age is a positive integer (greater than or equal to MIN_AGE_YEARS)
+ *
+ * @return patient age
+ */
+int getPatientAge()
+{
+    int age;
+
+    age = INVALID_INPUT;
+
+    do
+    {
+        printf("Enter patient age: ");
+
+        int input = scanf("%d", &age);
+
+        if(input != READ_SUCCESS ||
+            age < MIN_AGE_YEARS)
+        {
+            printf("Invalid age! Please enter a positive integer.\n");
+        }
+
+        clearInputBuffer();
+    }
+    while(age < MIN_AGE_YEARS);
+
+    return age;
+}
+
+/**
+ * Prompts user for patient room number, with checks to ensure room number is valid
+ * according to the following criteria:
+ * 1) Patient ID is a positive integer (greater than or equal to ROOM_NUMBER_MIN)
+ *
+ * @return patient room number
+ */
+int getPatientRoomNumber()
+{
+    int roomNumber;
+
+    roomNumber = INVALID_INPUT;
+
+    do
+    {
+        printf("Enter patient room number: ");
+
+        int input = scanf("%d", &roomNumber);
+
+        if(input != READ_SUCCESS ||
+            roomNumber < ROOM_NUMBER_MIN)
+        {
+            printf("Invalid room number! Please enter a positive integer.\n");
+        }
+
+        clearInputBuffer();
+    }
+    while(roomNumber < ROOM_NUMBER_MIN);
+
+    return roomNumber;
+}
+
+/**
  * Prompts the user to choose between two different search criteria:
- * 1) search by patient ID and 2) search by patient name. Then, checks if
- * patient exists in the database and printing the requested record.
+ * 1) Search by patient ID
+ * 2) Search by patient name.
+ * Then, checks if patient exists in the database and prints the requested record.
  */
 void searchForPatientRecord()
 {
-    int choice;
-    int id;
+    int sel;
     int index;
-    char name[NAME_MAX_CHAR];
 
-    index = ID_NOT_FOUND;
-
-    printf("Search By:\n");
-    printf("1. Patient ID\n");
-    printf("2. Patient Name\n");
-    scanf("%d", &choice);
-    getchar();
-
-    if(choice == SEARCH_BY_ID)
+    do
     {
-        printf("Enter Patient ID: ");
-        scanf("%d", &id);
-        getchar();
-        index = idExists(patientRecords, totalPatients, id);
-    }
-    else if(choice == SEARCH_BY_NAME)
-    {
-        printf("Enter Patient Name: ");
-        fgets(name, NAME_MAX_CHAR, stdin);
-        name[strcspn(name, "\n")] = TERMINAL_CHAR;
+        index = INVALID_INPUT;
 
-        for(int i = 0; i < totalPatients; i++)
+        printf("Search By:\n");
+        printf("1. Patient ID\n");
+        printf("2. Patient Name\n");
+        printf("3. Return to Menu\n");
+
+        sel = getSelection();
+
+        switch(sel)
         {
-            if (strcmp(patientRecords[i].name, name) == RECORD_FOUND)
-            {
-                index = i;
+            case SEARCH_BY_ID:
+                index = searchPatientByID();
                 break;
-            }
+            case SEARCH_BY_NAME:
+                index = searchPatientByName();
+                break;
+            case RETURN_TO_MENU:
+                printf("Returning to menu...\n");
+                break;
+            default:
+                printf("Invalid input! Try again.\n");
+        }
+
+        if (index != INVALID_INPUT)
+        {
+            printf("%-5s%-20s%-5s%-20s%-5s\n",
+                   "ID",
+                   "Name",
+                   "Age",
+                   "Diagnosis",
+                   "Room Number");
+            printPatientRecord(index);
+        }
+        else if (sel != RETURN_TO_MENU)
+        {
+            printf("Patient not found.\n");
+        }
+    }
+    while (sel != RETURN_TO_MENU);
+}
+
+/**
+ * Helper method to search the patientRecord array for the patient record corresponding
+ * to the specified ID.
+ *
+ * @return index of patient record corresponding to the specified ID
+ */
+int searchPatientByID()
+{
+    int index;
+    int id;
+
+    id = getPatientID();
+    index = idExists(patientRecords, totalPatients, id);
+
+    return index;
+}
+
+/**
+ * Helper method to search the patientRecord array for the patient record corresponding
+ * to the specified name.
+ *
+ * @return index of patient record corresponding to the specified name
+ */
+int searchPatientByName()
+{
+    char name[NAME_MAX_CHAR];
+    int index;
+
+    index = INVALID_INPUT;
+
+    printf("Enter Patient Name: ");
+    fgets(name, NAME_MAX_CHAR, stdin);
+    name[strcspn(name, "\n")] = TERMINAL_CHAR;
+
+    for(int i = 0; i < totalPatients; i++)
+    {
+        if (strcmp(patientRecords[i].name, name) == RECORD_FOUND)
+        {
+            index = i;
+            break;
         }
     }
 
-    if (index != ID_NOT_FOUND)
-    {
-        printf("%-5s%-20s%-5s%-20s%-5s\n",
-               "ID",
-               "Name",
-               "Age",
-               "Diagnosis",
-               "Room Number");
-        printPatientRecord(index);
-    }
-    else
-    {
-        printf("Patient not found.\n");
-    }
+    return index;
 }
 
 /**
@@ -238,4 +370,12 @@ void viewAllPatientRecords()
     {
         printPatientRecord(i);
     }
+}
+
+/**
+ * Helper method to flush the input buffer in the case of invalid inputs.
+ */
+void clearInputBuffer()
+{
+    while(getchar() != '\n');
 }
