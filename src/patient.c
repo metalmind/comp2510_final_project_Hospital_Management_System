1f63deaf8914a2591700ca1fca6f8b2c31c2c564
Owen Wou <19198526+metalmind@users.noreply.github.com>
2025-02-10 19:06:24 -0800

    docs: move javadocs from patient.c to patient.h

--- src/patient.c
+++ src/patient.c
@@ -6,23 +6,9 @@
 #include <stdio.h>
 #include <string.h>
 
-
 patient patientRecords[MAX_PATIENTS] = {};
 int totalPatients                    = 0;
 
-
-/**
- * Searches for a patientID in the array and returns its index if found;
- * otherwise, it returns ID_NOT_FOUND.
- *
- * @param arr Array of Patient Structs
- * @param size The number of patient records in the array as an int
- * @param id The ID we want to check for existence
- * @return id index if found, otherwise ID_NOT_FOUND
- *
- * @author Ayesha Anzer
- * @author Owen Wou
- */
 int idExists(patient arr[],
              int size,
              int id)
@@ -37,11 +23,6 @@ int idExists(patient arr[],
     return ID_NOT_FOUND;
 }
 
-/**
- * Adds a new patient to the patientRecord array.
- * @author Ayesha Anzer
- * @author Owen Wou
- */
 void addNewPatientRecord(void)
 {
     if(totalPatients >= MAX_PATIENTS)
@@ -88,16 +69,6 @@ void addNewPatientRecord(void)
     printf("Patient added successfully!\n");
 }
 
-/**
- * Prompts user for patient ID, with checks to ensure patient ID is valid
- * according to the following criteria:
- * 1) Patient ID is a positive integer
- * 2) Patient ID is within valid range - i.e., greater than or equal to ID_MIN_VALUE
- *    and less than or equal to ID_MAX_VALUE
- * 3) Patient with the specified ID does not already exist
- *
- * @return patient ID
- */
 int getUniquePatientID()
 {
     int id;
@@ -135,15 +106,6 @@ int getUniquePatientID()
     return id;
 }
 
-/**
- * Prompts user for patient ID, with checks to ensure patient ID is valid
- * according to the following criteria:
- * 1) Patient ID is a positive integer
- * 2) Patient ID is within valid range - i.e., greater than or equal to ID_MIN_VALUE
- *    and less than or equal to ID_MAX_VALUE
- *
- * @return patient ID
- */
 int getPatientID()
 {
     int id;
@@ -170,15 +132,6 @@ int getPatientID()
     return id;
 }
 
-/**
- * Prompts user for patient age, with checks to ensure patient age is valid
- * according to the following criteria:
- * 1) Patient age is a positive integer
- * 2) Patient age is within valid range - i.e., greater than or equal to MIN_AGE_YEARS
- *    and less than or equal to MAX_AGE_YEARS
- *
- * @return patient age
- */
 int getPatientAge()
 {
     int age;
@@ -205,15 +158,6 @@ int getPatientAge()
     return age;
 }
 
-/**
- * Prompts user for patient room number, with checks to ensure room number is valid
- * according to the following criteria:
- * 1) Room number is a positive integer
- * 2) Room number is within valid range - i.e., greater than or equal to ROOM_NUMBER_MIN
- *    and less than or equal to ROOM_NUMBER_MAX
- *
- * @return patient room number
- */
 int getPatientRoomNumber()
 {
     int roomNumber;
@@ -240,15 +184,6 @@ int getPatientRoomNumber()
     return roomNumber;
 }
 
-/**
- * Wrapper function for scanf() which takes a prompt message to print and
- * a value to which to assign the input. Clears the input buffer in case of invalid
- * input.
- *
- * @param prompt prompt message
- * @param input value to assign input
- * @return number of items successfully read
- */
 int getInput(char* prompt,
              int* input)
 {
@@ -263,17 +198,6 @@ int getInput(char* prompt,
     return numItemsRead;
 }
 
-/**
- * Validates whether input value is of valid type (i.e., has been successfully read
- * and assigned) and within range.
- *
- * @param numItemsRead number of items successfully read
- * @param input data value to validate
- * @param lowerBound lower bound of valid range
- * @param upperBound upper bound of valid range
- * @param errorMessage error message to print should data be invalid
- * @return 1 is data value is valid, otherwise 0
- */
 int validateData(int numItemsRead,
                  int input,
                  int lowerBound,
@@ -295,14 +219,6 @@ int validateData(int numItemsRead,
     return valid;
 }
 
-/**
- * Validates whether input read was successful - checks that numItemsRead
- * is equal to READ_SUCCESS, indicating that an integer value was successfully
- * extracted and assigned.
- *
- * @param numItemsRead number of items successfully read
- * @return 1 if input type is valid, otherwise 0
- */
 int validateInputType(const int numItemsRead)
 {
     int valid;
@@ -317,14 +233,6 @@ int validateInputType(const int numItemsRead)
     return valid;
 }
 
-/**
- * Validates whether number is within valid range.
- *
- * @param num number to validate
- * @param lowerBound lower bound of valid range
- * @param upperBound upper bound of valid range
- * @return 1 if number is within valid range, otherwise 0
- */
 int validateNum(const int num,
                 const int lowerBound,
                 const int upperBound)
@@ -341,12 +249,6 @@ int validateNum(const int num,
     return valid;
 }
 
-/**
- * Prompts the user to choose between two different search criteria:
- * 1) Search by patient ID
- * 2) Search by patient name.
- * Then, checks if patient exists in the database and prints the requested record.
- */
 void searchForPatientRecord()
 {
     int sel;
@@ -366,9 +268,6 @@ void searchForPatientRecord()
     while(sel != RETURN_TO_MENU);
 }
 
-/**
- * Prints the patient menu.
- */
 void printPatientMenu()
 {
     printf("Search By:\n");
@@ -377,17 +276,6 @@ void printPatientMenu()
     printf("3. Return to Menu\n");
 }
 
-/**
- * Routes to the search method associated with the selection.
- * If the selection is not available, prompts the user to try again.
- *
- * Search By:
- *    1. Patient ID
- *    2. Patient Name
- *    3. Return to Menu
- *
- * @param sel selected menu item
- */
 int searchCriteriaSelection(int sel)
 {
     int index = INVALID_INPUT;
@@ -410,14 +298,6 @@ int searchCriteriaSelection(int sel)
     return index;
 }
 
-/**
- * If requested patient record is found, prints its details. Otherwise,
- * if user menu selection was either SEARCH_BY_ID or SEARCH_BY_NAME
- * (i.e., was not an invalid selection), prints that record was not found.
- *
- * @param index index of requested patient record in the patientRecord array
- * @param sel user menu selection
- */
 void handlePatientSearchResult(int index,
                                int sel)
 {
@@ -437,12 +317,6 @@ void handlePatientSearchResult(int index,
     }
 }
 
-/**
- * Helper method to search the patientRecord array for the patient record corresponding
- * to the specified ID.
- *
- * @return index of patient record corresponding to the specified ID
- */
 int searchPatientByID()
 {
     int index;
@@ -456,12 +330,6 @@ int searchPatientByID()
     return index;
 }
 
-/**
- * Helper method to search the patientRecord array for the patient record corresponding
- * to the specified name.
- *
- * @return index of patient record corresponding to the specified name
- */
 int searchPatientByName()
 {
     char name[NAME_MAX_CHAR];
@@ -489,10 +357,6 @@ int searchPatientByName()
     return index;
 }
 
-/**
- * Prints the patient record corresponding to the specified patient ID.
- * @param id patient id
- */
 void printPatientRecord(int id)
 {
     printf("%-5d%-20s%-5d%-20s%-5d\n",
@@ -503,9 +367,6 @@ void printPatientRecord(int id)
            patientRecords[id].roomNumber);
 }
 
-/**
- * Prints all patient records in the patientRecord array.
- */
 void viewAllPatientRecords()
 {
     if(totalPatients == 0)
@@ -528,9 +389,6 @@ void viewAllPatientRecords()
     }
 }
 
-/**
- * Helper method to flush the input buffer in the case of invalid inputs.
- */
 void clearInputBuffer()
 {
     while(getchar() != '\n');
--- src/patient.h
+++ src/patient.h
@@ -44,42 +44,163 @@ struct patient
     int roomNumber;
 };
 
+/**
+ * Adds a new patient to the patientRecord array.
+ * @author Ayesha Anzer
+ * @author Owen Wou
+ */
 void addNewPatientRecord(void);
 
+/**
+ * Searches for a patientID in the array and returns its index if found;
+ * otherwise, it returns ID_NOT_FOUND.
+ *
+ * @param arr Array of Patient Structs
+ * @param size The number of patient records in the array as an int
+ * @param id The ID we want to check for existence
+ * @return id index if found, otherwise ID_NOT_FOUND
+ *
+ * @author Ayesha Anzer
+ * @author Owen Wou
+ */
 int idExists(patient arr[],
              int size,
              int id);
 
+/**
+ * Prompts user for patient ID, with checks to ensure patient ID is valid
+ * according to the following criteria:
+ * 1) Patient ID is a positive integer
+ * 2) Patient ID is within valid range - i.e., greater than or equal to ID_MIN_VALUE
+ *    and less than or equal to ID_MAX_VALUE
+ * 3) Patient with the specified ID does not already exist
+ *
+ * @return patient ID
+ */
 int getUniquePatientID(void);
 
+/**
+ * Prompts user for patient ID, with checks to ensure patient ID is valid
+ * according to the following criteria:
+ * 1) Patient ID is a positive integer
+ * 2) Patient ID is within valid range - i.e., greater than or equal to ID_MIN_VALUE
+ *    and less than or equal to ID_MAX_VALUE
+ *
+ * @return patient ID
+ */
 int getPatientID(void);
 
+/**
+ * Prompts user for patient age, with checks to ensure patient age is valid
+ * according to the following criteria:
+ * 1) Patient age is a positive integer
+ * 2) Patient age is within valid range - i.e., greater than or equal to MIN_AGE_YEARS
+ *    and less than or equal to MAX_AGE_YEARS
+ *
+ * @return patient age
+ */
 int getPatientAge(void);
 
+/**
+ * Prompts user for patient room number, with checks to ensure room number is valid
+ * according to the following criteria:
+ * 1) Room number is a positive integer
+ * 2) Room number is within valid range - i.e., greater than or equal to ROOM_NUMBER_MIN
+ *    and less than or equal to ROOM_NUMBER_MAX
+ *
+ * @return patient room number
+ */
 int getPatientRoomNumber(void);
 
+/**
+ * Prompts the user to choose between two different search criteria:
+ * 1) Search by patient ID
+ * 2) Search by patient name.
+ * Then, checks if patient exists in the database and prints the requested record.
+ */
 void searchForPatientRecord(void);
 
+/**
+ * Routes to the search method associated with the selection.
+ * If the selection is not available, prompts the user to try again.
+ *
+ * Search By:
+ *    1. Patient ID
+ *    2. Patient Name
+ *    3. Return to Menu
+ *
+ * @param sel selected menu item
+ */
 int searchCriteriaSelection(int sel);
 
+/**
+ * If requested patient record is found, prints its details. Otherwise,
+ * if user menu selection was either SEARCH_BY_ID or SEARCH_BY_NAME
+ * (i.e., was not an invalid selection), prints that record was not found.
+ *
+ * @param index index of requested patient record in the patientRecord array
+ * @param sel user menu selection
+ */
 void handlePatientSearchResult(int index,
                                int sel);
 
+/**
+ * Helper method to search the patientRecord array for the patient record corresponding
+ * to the specified ID.
+ *
+ * @return index of patient record corresponding to the specified ID
+ */
 int searchPatientByID(void);
 
+/**
+ * Helper method to search the patientRecord array for the patient record corresponding
+ * to the specified name.
+ *
+ * @return index of patient record corresponding to the specified name
+ */
 int searchPatientByName(void);
 
+/**
+ * Prints the patient record corresponding to the specified patient ID.
+ * @param id patient id
+ */
 void printPatientRecord(int id);
 
+/**
+ * Prints all patient records in the patientRecord array.
+ */
 void viewAllPatientRecords(void);
 
+/**
+ * Prints the patient menu.
+ */
 void printPatientMenu();
 
 // TOOLS?
+/**
+ * Wrapper function for scanf() which takes a prompt message to print and
+ * a value to which to assign the input. Clears the input buffer in case of invalid
+ * input.
+ *
+ * @param prompt prompt message
+ * @param input value to assign input
+ * @return number of items successfully read
+ */
 int getInput(char* prompt,
              int* input);
 
 //TOOLS
+/**
+ * Validates whether input value is of valid type (i.e., has been successfully read
+ * and assigned) and within range.
+ *
+ * @param numItemsRead number of items successfully read
+ * @param input data value to validate
+ * @param lowerBound lower bound of valid range
+ * @param upperBound upper bound of valid range
+ * @param errorMessage error message to print should data be invalid
+ * @return 1 is data value is valid, otherwise 0
+ */
 int validateData(int numItemsRead,
                  int input,
                  int lowerBound,
@@ -87,14 +208,33 @@ int validateData(int numItemsRead,
                  char* errorMessage);
 
 //TOOLS
+/**
+ * Validates whether input read was successful - checks that numItemsRead
+ * is equal to READ_SUCCESS, indicating that an integer value was successfully
+ * extracted and assigned.
+ *
+ * @param numItemsRead number of items successfully read
+ * @return 1 if input type is valid, otherwise 0
+ */
 int validateInputType(int numItemsRead);
 
 //TOOLS
+/**
+ * Validates whether number is within valid range.
+ *
+ * @param num number to validate
+ * @param lowerBound lower bound of valid range
+ * @param upperBound upper bound of valid range
+ * @return 1 if number is within valid range, otherwise 0
+ */
 int validateNum(int num,
                 int lowerBound,
                 int upperBound);
 
 // TOOLS
+/**
+ * Helper method to flush the input buffer in the case of invalid inputs.
+ */
 void clearInputBuffer(void);
 
 
