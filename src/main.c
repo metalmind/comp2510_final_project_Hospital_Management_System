9b3689d7c3f93f2d196246c19d766bb1a52cc3c5
Owen Wou <19198526+metalmind@users.noreply.github.com>
2025-02-15 14:47:12 -0800

    feat: add basic scheduling functionality

--- src/main.c
+++ src/main.c
@@ -1,14 +1,23 @@
-#include "menu.h"
-#include "patient.h"
+//#include "menu.h"
+//#include "patient.h"
+#include<stdio.h>
+#include<String.h>
+//#include "tools.h"
+#include "schedule.h"
+
 
 int main(void)
 {
-    addNewPatientRecord();
-    addNewPatientRecord();
-    viewAllPatientRecords();
-    searchForPatientRecord();
+    printDocWeekSchedule();
+    addToSchedule("Dr. Shepard",
+                  SUN,
+                  MORNING);
+    printDocWeekSchedule();
+
+    // addNewPatientRecord();
+    // addNewPatientRecord();
+    // viewAllPatientRecords();
+    // searchForPatientRecord();
 
     return 0;
 }
-
-
---
+++ src/schedule.h
@@ -0,0 +1,80 @@
+/**
+ * The schedule handles all tasks related to doctors being added and viewing the
+ * schedule.
+ *
+ * @author Owen Wou
+ * @version February 2025
+ */
+#ifndef SCHEDULE_H
+#define SCHEDULE_H
+
+#define DAYS_IN_WEEK 7
+#define NUM_SHIFTS 3
+#define MAX_CHAR_SHIFT 15
+
+#define MAX_DOCTORS 10
+
+enum daysInWeek { SUN, MON, TUE, WED, THU, FRI, SAT };
+
+enum shift { MORNING, AFTERNOON, EVENING };
+
+/**
+ * Takes in the enum daysInWeek and sets dayOfWeekName to the string version.
+ *
+ * @param dayOfWeek enum daysInWeek
+ * @param dayOfWeekName The String to populate.
+ */
+void getDayOfWeekNameStr(const enum daysInWeek dayOfWeek,
+                         char* dayOfWeekName);
+
+/**
+ * Takes in the enum shift and sets of the shift name to the string version.
+ *
+ * @param shift enum shift
+ */
+void getShiftNameStr(enum shift shift,
+                     char* shiftName);
+
+/**
+ * Print the schedule of the doctors for the daysInWeek.
+ */
+void printDocWeekSchedule();
+
+/**
+ * Adds a doctor to the schedule.
+ *
+ * @param doctorName Name of the doctor to add.
+ * @param dayOfWeek The day to schedule the doctor on.
+ * @param shiftToFill The shift you want to fill.
+ */
+void addToSchedule(char* doctorName,
+                   enum daysInWeek dayOfWeek,
+                   enum shift shiftToFill);
+
+/**
+ * Helper function to that prints the crossbar for the schedule to the screen.
+ */
+void printScheduleDivider();
+
+/**
+ * Helper function to print the day of the week header to the screen.
+ */
+void printDayOfWeekHeader();
+
+/**
+ * Helper function that prints the shifts for the week to the screen.
+ */
+void printShiftsForWeek();
+
+/**
+ * Gets the name for the doctor on schedule returned through the doctor parameter.
+ *
+ * @param dayOfWeek The day of the week to check.
+ * @param shift The shift you want to check
+ * @param doctor The return value that will have the name of the doctor on shift.
+ */
+void getDoctorOnShift(enum daysInWeek dayOfWeek,
+                      enum shift shift,
+                      char* doctor);
+
+#endif //SCHEDULE_H
