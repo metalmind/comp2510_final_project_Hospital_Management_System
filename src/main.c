c93a610e6da27ba01dde37d652bf6793ecd16031
Owen Wou <19198526+metalmind@users.noreply.github.com>
2025-02-17 22:20:46 -0800

    refactor: remove tests from menu and add single call to menu()

--- src/main.c
+++ src/main.c
@@ -1,43 +1,9 @@
-//#include "menu.h"
-#include "../inc/patient.h"
-#include<stdio.h>
-#include<string.h>
-//#include "tools.h"
-#include "../inc/doctor.h"
-#include "../inc/schedule.h"
+#include "..\inc\menu.h"
+
 
 int main(void)
 {
-    printDocWeekSchedule();
-
-    addNewDoctorRecord();
-
-    doctor* shepard = getDoctor(0);
-
-    addToSchedule (shepard,
-                  SUN,
-                  MORNING);
-
-    addToSchedule (shepard,
-                  TUE,
-                  EVENING);
-
-    printDocWeekSchedule();
-
-    addNewDoctorRecord();
-    addNewDoctorRecord();
-    viewAllDoctorRecords();
-    fireDoctor();
-    viewAllDoctorRecords();
-
-    printDocWeekSchedule();
-
-    addNewPatientRecord();
-    addNewPatientRecord();
-    viewAllPatientRecords();
-    searchForPatientRecord();
-    dischargePatient();
-    viewAllPatientRecords();
+    menu();
 
     return 0;
 }
